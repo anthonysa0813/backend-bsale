25541d817a532935501d1d2ae55a96efcd1beba6
ever <ever864@gmail.com>
2023-02-01 15:55:55 -0500

    Add authentication for api admin

--- models/auth/index.js
+++ models/auth/index.js
@@ -30,6 +30,38 @@ UserSchema.methods.toJSON = function () {
   };
 };
 
+const AdminSchema = Schema({
+  name: {
+    type: String,
+    require: false,
+  },
+  status: {
+    type: Boolean,
+    require: false,
+    default: false,
+  },
+  email: {
+    type: String,
+    require: [true, "el email es obligatorio"],
+  },
+  password: {
+    type: String,
+    require: [true, "el password es obligatorio"],
+  },
+});
+
+AdminSchema.methods.toJSON = function () {
+  const { _id, password, name, status, email } = this.toObject();
+  return {
+    uid: _id,
+    email,
+    name,
+    status,
+  };
+};
+
 const User = model("User", UserSchema);
+const Admin = model("Admin", AdminSchema);
 
 module.exports = User;
+module.exports = Admin;
