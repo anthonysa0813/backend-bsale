e0736e9a1f8daac155600ce7ee0bf2d58f7a0422
anthony <anthonysa0813@gmail.com>
2023-02-19 10:44:28 -0500

    added the controller addScore to run-code

--- routes/answer/index.js
+++ routes/answer/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { validateAnswer } = require("../../controller/answer");
+const { validateAnswer, addScore } = require("../../controller/answer");
 
 const router = Router();
 
 router.post("/validateAnswer/:idQuestion/:idUser", validateAnswer);
+
+router.get("/addScore/:idUser", addScore);
 
 module.exports = router;
